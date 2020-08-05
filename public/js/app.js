c40e9ca29bad288f107dbd46235416f94a01c6c0
unknown <oleg-vasilyevv@yandex.ru>
2020-08-05 12:02:08 +0300

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('This is js file')
-
 const form = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne  = document.querySelector('#message-1')
